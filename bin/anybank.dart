ad012ad8a2a565cec523bdb4b00b09dd6851ab6f
Caroline <carol-html@hotmail.com>
2025-11-14 18:02:54 -0300

    chore: separar classe Conta em arquivo próprio

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -26,17 +26,3 @@ print("");
 
 }
 
-class Conta{
-  String titular;
-  double saldo;
-
-  Conta(this.titular, this.saldo);
-
-  void receber(double valor){
-  saldo += valor;
-  }
-
-  void enviar(double valor){
-  saldo -= valor;
-  }
-}
---
+++ lib/conta.dart
@@ -0,0 +1,14 @@
+class Conta{
+  String titular;
+  double saldo;
+
+  Conta(this.titular, this.saldo);
+
+  void receber(double valor){
+  saldo += valor;
+  }
+
+  void enviar(double valor){
+  saldo -= valor;
+  }
+}
